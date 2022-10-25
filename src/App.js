6c3c53688328e65dda4bdf729972afec1bcdcafc
fateme <abedifateme1378@gmail.com>
2022-10-26 00:53:33 +0330

    App.js updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import AddTodoForm from './components/AddTodoForm';
+import { css } from '@emotion/react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div css={css`
+    
+    background-color: #777;
+    font-size: 24px;
+    border-radius: 4px;
+    color:#ddd;
+    display:flex;
+    align-items: center;
+    
+  `}>
+    
+    <AddTodoForm/>
+
     </div>
-  );
+  )
+  
 }
 
 export default App;
